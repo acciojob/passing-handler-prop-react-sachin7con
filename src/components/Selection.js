5623c6996b9de4850fca7353999265c9ea5d9d48
sachin7con <sachin7con@gmail.com>
2024-06-06 17:38:50 +0530

    Uploaded after updation

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,22 @@
+// SGN
+import React, { useState } from 'react';
 
+const Selection = ({ applyColor }) => {
+  const [background, setBackground] = useState({ background: "" });
+
+  const updateSelectionStyle = (newBackground) => {
+    setBackground(newBackground);
+  };
+
+  return (
+    <div
+      className="fix-box"
+      style={{ background: background.background }}
+      onClick={() => applyColor(updateSelectionStyle)}
+    >
+    <div className="selection-text">Selection</div>
+    </div>
+  );
+}
+
+export default Selection;
